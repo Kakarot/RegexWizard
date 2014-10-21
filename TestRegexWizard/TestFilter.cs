afb6be6e386b2628a06eb727df8237d10c06a731
Kakarot <JamesRuiz@gatech.edu>
2014-10-20 21:39:24 -0400

    Init Commit

---
+++ TestRegexWizard/TestFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestRegexWizard
+{
+    [TestClass]
+    public class TestFilter
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
